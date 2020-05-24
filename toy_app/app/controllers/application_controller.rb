9e43f9674016668dcaf63e26ba90631749b71ca2
n-hata91 <htnoms5a@gmail.com>
2020-05-24 16:55:44 +0900

    finish toy_app

--- toy_app/app/controllers/application_controller.rb
+++ toy_app/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-  render html: 'Hi, toy_app'
-
+    render html: 'Hi, toy_app'
+  end
   protect_from_forgery with: :exception
 end
