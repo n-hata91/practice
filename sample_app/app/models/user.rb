f1369c9e5f3e522460da0240031a6352f4089c3e
n-hata91 <htnoms5a@gmail.com>
2020-05-28 21:13:19 +0900

    add signup function

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase}#saveする前に小文字に変える。右辺のself.は同じモデル内なので省略できる
   validates :name, presence: true, length: {maximum:50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum:255},
               format: { with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false}
-  
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6}
+
 end
