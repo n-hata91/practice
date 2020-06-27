0498e90f6d7f8e28b504ef39e5c8c3bac9e1d560
n-hata91 <htnoms5a@gmail.com>
2020-06-27 21:05:29 +0900

    test

--- myblog/config/routes.rb
+++ myblog/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
-  resources :posts do
+  resources :posts do# パーシャルでも活躍
     resources :comments, only: [:create, :destroy]
   end
 end
